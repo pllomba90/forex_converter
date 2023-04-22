949ddfed5b4c85ee63b3bd0ac64aaf3860302c6d
Pete <peterllombardo@gmail.com>
2023-04-22 17:20:15 -0400

    initial code commits

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 ### Base  application code
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = 'dolladollabills'
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
