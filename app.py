d92bd5e7d5bcbf2c14126ffdae0efc12ebc88ac2
Peter Lombardo <peterllombardo@gmail.com>
2023-05-01 09:41:46 -0400

    code commits

--- app.py
+++ app.py
@@ -1,12 +1,65 @@
-### Base  application code
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
+from typing import List
+import requests
+import json
+
+
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = 'dolladollabills'
 debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
+def validate_inputs(amount: str, starting_currency: str, converting_currency: str, codes: List[str]) -> bool:
+    if not amount.isdigit():
+        flash('Amount must be an integer')
+        return False
+    if starting_currency not in codes:
+        flash('Starting currency code not found')
+        return False
+    if converting_currency not in codes:
+        flash('Converting currency code not found')
+        return False
+    return True
+
+@app.route('/get-currencies')
+def get_currencies():
+     url = "https://api.exchangerate.host/symbols"
+     response = requests.get(url)
+     data = json.loads(response.text)
+     symbols = data["symbols"]
+     codes = list(symbols.keys())
+     return codes
+
+@app.route('/get_descriptions')
+def get_descriptions():
+     url = "https://api.exchangerate.host/symbols"
+     response = requests.get(url)
+     data = json.loads(response.text)
+     descriptions = {key: value['description'] for key, value in data['symbols'].items()}
+     return descriptions
+    
+@app.route('/convert-currencies', methods=["GET"])
+def convert_currencies():
+    starting_currency = request.args.get('starting_currency')
+    amount = request.args.get('amount')
+    converting_currency = request.args.get('converting_currency')
+    descriptions = get_descriptions()
+    codes = get_currencies()
+    if validate_inputs(amount, starting_currency, converting_currency, codes):
+        url = f'https://api-us.exchangerate.host/convert?from={starting_currency}&to={converting_currency}&amount={amount}'
+        response = requests.get(url)
+        init_result = response.json()
+        result = init_result['result']
+        converted_amount = round(result, 2)
+        converting_description = descriptions[converting_currency]
+        return render_template('result.html', converted_amount=converted_amount, converting_description=converting_description)
+    else:
+     return redirect('/')
+    
 @app.route('/')
 def home_page():
-    return render_template("index.html")
+    codes = get_currencies()
+    return render_template("index.html", codes=codes)
--- test.py
+++ test.py
@@ -1,1 +1,45 @@
-### Here is where I will write my test code
+import unittest
+import requests
+from app import app, get_currencies, get_descriptions, convert_currencies, validate_inputs
+
+class TestApp(unittest.TestCase):
+
+    def test_validate_inputs(self):
+        codes = ["USD", "EUR", "GBP"]
+        self.assertTrue(validate_inputs("100", "USD", "EUR", codes))
+        self.assertFalse(validate_inputs("100", "JPY", "EUR", codes))
+        self.assertFalse(validate_inputs("abc", "USD", "EUR", codes))
+        self.assertFalse(validate_inputs("100", "USD", "JPY", codes))
+
+    def test_get_currencies(self):
+        with app.test_client() as client:
+            response = client.get('/get-currencies')
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(response.json, list)
+            self.assertIn('USD', response.json)
+            self.assertIn('EUR', response.json)
+
+    def test_get_descriptions(self):
+        with app.test_client() as client:
+            response = client.get('/get_descriptions')
+            self.assertEqual(response.status_code, 200)
+            self.assertIsInstance(response.json, dict)
+            self.assertIn('USD', response.json)
+            self.assertEqual(response.json['USD'], 'United States Dollar')
+            self.assertIn('EUR', response.json)
+            self.assertEqual(response.json['EUR'], 'Euro')
+
+    def test_convert_currencies(self):
+        with app.test_client() as client:
+            response = client.get('/convert-currencies?starting_currency=USD&amount=10&converting_currency=EUR')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'converted amount:', response.data)
+            self.assertIn(b'8.32', response.data)
+            self.assertIn(b'Euro', response.data)
+
+    def test_convert_currencies_invalid_amount(self):
+        with app.test_client() as client:
+            response = client.get('/convert-currencies?starting_currency=USD&amount=invalid&converting_currency=EUR')
+            self.assertEqual(response.status_code, 302)
+            response = client.get('/convert-currencies?starting_currency=USD&amount=-10&converting_currency=EUR')
+            self.assertEqual(response.status_code, 302)
